3185acd84dc21073ed09937e94de743716cd3e52
Omir-zak <132219518+Omir-zak@users.noreply.github.com>
2023-07-22 02:43:36 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import router from "./router.js"
+import fileupload  from 'express-fileupload';
+
+const PORT = 5000;
+const DB_URL='mongodb+srv://Omirzak:MongoDBaccountBaxa1@cluster0.8vnnphz.mongodb.net/?retryWrites=true&w=majority'
+const app = express()
+
+app.use(express.json())
+app.use(fileupload({}))
+app.use('/api', router)
+ 
+async function startApp(){
+    try {
+        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+        app.listen(PORT, () => console.log('SERVER STARTED ON PORT '+ PORT))
+    }   catch(e){
+        console.log(e)
+    }
+}
+startApp()
